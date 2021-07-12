8c3d9c60d44fa1e8f06ae645fb70c97c681b4386
rezasalehi <rezasalehidev@gmail.com>
2021-07-12 13:29:00 +0430

    Updates

---
+++ precache-manifest.75f1d651e6239bbca08d1ead189b9138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26d5b6c3bf1fa25ce96845ff8edf3c0",
+    "url": "/sliderReact/index.html"
+  },
+  {
+    "revision": "15746181eff64a9be409",
+    "url": "/sliderReact/static/css/main.9648fb74.chunk.css"
+  },
+  {
+    "revision": "4acce97ee23a5c46634a",
+    "url": "/sliderReact/static/js/2.6e9d5766.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sliderReact/static/js/2.6e9d5766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15746181eff64a9be409",
+    "url": "/sliderReact/static/js/main.951f12d3.chunk.js"
+  },
+  {
+    "revision": "fd647dc2a11c8bf2e417",
+    "url": "/sliderReact/static/js/runtime-main.d5eb3ca0.js"
+  }
+]);
