47b04e4889e018bf6f357b70420364e45bf133f6
rezasalehi <rezasalehidev@gmail.com>
2021-07-12 13:27:08 +0430

    deploy project slider react to git

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    name: " شماره 1",
+    title: "مهندس",
+    quote: "توضیحات شماره اول",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: " شماره 2 ",
+    title: "مکانیک",
+    quote: "توضیحات شماره دوم",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "شماره 3 ",
+    title: "طراح",
+    quote: "توضیحات شماره سوم",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "شماره 4",
+    title: "پزشک",
+    quote: "توضیحات شماره چهارم",
+  },
+];
+
+export default people;
